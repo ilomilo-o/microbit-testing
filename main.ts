cd65633eece66d0a5a8962b780c06e4fca5241f2
ilomilo-o <120426842+ilomilo-o@users.noreply.github.com>
2022-12-12 15:04:30 -0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,40 @@
+input.onButtonPressed(Button.A, function () {
+    for (let index = 0; index < 4; index++) {
+        basic.showLeds(`
+            . . . . .
+            . . . . #
+            . . . . #
+            . . . . .
+            . . . . .
+            `)
+        basic.showLeds(`
+            . . . . #
+            . . . # #
+            . . . # #
+            . . . . #
+            . . . . .
+            `)
+        basic.showLeds(`
+            . . . # .
+            . . # # #
+            . . # # #
+            . . . # #
+            . . . . #
+            `)
+        basic.showLeds(`
+            . . # . #
+            . # # # #
+            . # # # #
+            . . # # #
+            . . . # .
+            `)
+        basic.showIcon(IconNames.Heart)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+    }
+})
